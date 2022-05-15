e105c10a43290f857368289e6fcb31293275d29c
Adam Trotta <ahtrotta@gmail.com>
2022-05-15 17:38:29 -0400

    feat: handle POST by rendering form data

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -1,4 +1,9 @@
 class UrlShortenerController < ApplicationController
   def index 
   end
+
+  def create
+    long_url = params[:long_url]
+    render html: "#{long_url}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'url_shortener#index'
+  post '/', to: 'url_shortener#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
