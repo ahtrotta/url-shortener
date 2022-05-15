205499164a702ba920bc667a6c4847b93e4c09e4
Adam Trotta <ahtrotta@gmail.com>
2022-05-15 19:24:46 -0400

    feat: added helper function to create short URLs

--- app/helpers/url_shortener_helper.rb
+++ app/helpers/url_shortener_helper.rb
@@ -1,2 +1,6 @@
 module UrlShortenerHelper
+  def create_short_url
+    # get current time in nanoseconds (since default Epoch) and convert to base 36
+    (Time.now.to_f * 10000000000.0).to_i.to_s(36)
+  end
 end
