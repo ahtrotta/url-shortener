1b1419bf1b46acf9beb12d6ae9b3914660956a86
Adam Trotta <ahtrotta@gmail.com>
2022-05-15 19:55:30 -0400

    feat: save URL associations to the database

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -4,6 +4,13 @@ class UrlShortenerController < ApplicationController
 
   def create
     long_url = params[:long_url]
-    render html: "#{long_url}"
+    short_url = helpers.create_short_url
+    @url_association = UrlAssociation.new(long_url: long_url, short_url: short_url)
+
+    if @url_association.save
+      render html: "#{root_url}#{short_url}"
+    else
+      render html: 'failed to save to database'
+    end
   end
 end
