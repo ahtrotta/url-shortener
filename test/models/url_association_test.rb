828684c50595a1672353f18e384f6105205b0c6b
Adam Trotta <ahtrotta@gmail.com>
2022-05-16 21:10:42 -0400

    test: added model tests for url_association

--- test/models/url_association_test.rb
+++ test/models/url_association_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class UrlAssociationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a url_association with a blank long URL" do
+    url_association = UrlAssociation.new(short_url: '123456789012')
+    assert_not url_association.save
+  end
+
+  test "should not save a url_association with an invalid URL" do
+    invalid_url_pipe = UrlAssociation.new(long_url: '|', short_url: '123456789012')
+    invalid_url_newline = UrlAssociation.new(long_url: '\n', short_url: '123456789012')
+    invalid_url_tab = UrlAssociation.new(long_url: '\t', short_url: '123456789012')
+    invalid_url_backslash = UrlAssociation.new(long_url: '\\', short_url: '123456789012')
+    assert_not invalid_url_pipe.save
+    assert_not invalid_url_newline.save
+    assert_not invalid_url_tab.save
+    assert_not invalid_url_backslash.save
+  end
+
+  test "should not save a url_association even if missing protocol" do
+    url_association = UrlAssociation.new(long_url: 'www.google.com', short_url: '123456789012')
+    assert_not url_association.save
+  end
 end
