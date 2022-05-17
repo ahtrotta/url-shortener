878ab5032835ae0a2e9f0e6a59d9e707dac1696c
Adam Trotta <ahtrotta@gmail.com>
2022-05-16 21:10:21 -0400

    test: added tests to url_associations_controller_test

--- test/controllers/url_associations_controller_test.rb
+++ test/controllers/url_associations_controller_test.rb
@@ -6,14 +6,19 @@ class UrlAssociationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "url_shortener#index should have a form that sends a POST to root" do
+  test "a GET request to root renders the application layout with URL Shortener heading" do
+    get root_url
+    assert_select 'h1', 'URL Shortener'
+  end
+
+  test "root page should have a form that sends a POST to root" do
     get root_url
     assert_select "form[action=?]", root_path
     assert_select "form[method=?]", 'post'
   end
 
-  test "should post to root" do
-    post root_url
-    assert_response :success
+  test "should post to root and redirect" do
+    post root_url, params: { long_url: 'https://www.google.com' }
+    assert_response :found
   end
 end
