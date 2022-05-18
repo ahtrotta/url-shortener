65749de88b481f90bce8d4814ad0f49d83eea5d9
Adam Trotta <ahtrotta@gmail.com>
2022-05-17 20:21:19 -0400

    test: added url_association model tests

--- test/models/url_association_test.rb
+++ test/models/url_association_test.rb
@@ -21,4 +21,24 @@ class UrlAssociationTest < ActiveSupport::TestCase
     url_association = UrlAssociation.new(long_url: 'www.google.com', short_url: '123456789012')
     assert_not url_association.save
   end
+
+  test "should be able to retrieve a url_association by id" do
+    @url_association = UrlAssociation.find 1
+    assert @url_association
+  end
+
+  test "should be able to retrieve a url_association by short_url" do
+    @url_association = UrlAssociation.find_by short_url: 'testShortUrl'
+    assert @url_association
+  end
+
+  test "should not be able to retrieve a url_association by id not in the database" do
+    @url_association = UrlAssociation.find 3 rescue false
+    assert_not @url_association
+  end
+
+  test "should not be able to retrieve a url_association by short_url not in the database" do
+    @url_association = UrlAssociation.find_by short_url: 'notPresent' rescue false
+    assert_not @url_association
+  end
 end
